6e82964091b1a91760ea4fbd3a1f48cc678bc089
Faadddi123 <fadi.mhamdi@uit.ac.ma>
2024-02-20 16:00:19 +0100

    google o dakshi " "

--- routes/web.php
+++ routes/web.php
@@ -1,6 +1,12 @@
 <?php
 
+use App\Http\Controllers\ProfileController;
+use App\Models\User;
 use Illuminate\Support\Facades\Route;
+use Laravel\Socialite\Facades\Socialite;
+use Illuminate\Support\Facades\Auth;
+use Illuminate\Http\Request;
+
 
 /*
 |--------------------------------------------------------------------------
@@ -13,6 +19,52 @@ use Illuminate\Support\Facades\Route;
 |
 */
 
+ 
+Route::get('/auth/redirect/{provider}', function (Request $request, $provider) {
+    
+    if (!in_array($provider, ['github', 'google', 'facebook', 'etc'])) {
+        abort(404); // Provider not found
+    }
+
+    return Socialite::driver($provider)->redirect();
+});
+
+Route::get('/auth/callback', function (Request $request) {
+    $provider = $request->query('provider');
+    $providerUser = Socialite::driver($provider)->user();
+    $user = User::updateOrCreate([
+        $provider . '_id' => $providerUser->id,
+    ], [
+        'name' => $providerUser->name,
+        'email' => $providerUser->email,
+    ]);
+
+    Auth::login($user);
+
+    return redirect('/dashboard');
+});
+
+
+
+
+
+
+
+
+
+
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
